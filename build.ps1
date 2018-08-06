9e54dc95145b6c52f60a3b09e6f168213c2ef204
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-08-05 21:24:35 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -3,3 +3,4 @@
 #(Get-Content ./client/package.json).replace('v1', '1.0.1') | Set-Content ./client/package.json
 Copy-Item ./cloudmersive_document_convert_api_client/* -Destination . -Recurse -Force
 Remove-Item –path ./cloudmersive_document_convert_api_client –recurse
+& php C:\ProgramData\ComposerSetup\bin\composer.phar install
