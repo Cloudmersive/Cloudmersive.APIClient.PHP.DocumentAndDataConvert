361c858dd78955f9ca590ea28e5ea5d6fc7ecc17
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2023-09-03 19:48:14 +0000

    3.1.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'cloudmersive/cloudmersive_document_convert_api_client',
-        'pretty_version' => '3.0.1',
-        'version' => '3.0.1.0',
+        'pretty_version' => '3.1.1',
+        'version' => '3.1.1.0',
         'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'cloudmersive/cloudmersive_document_convert_api_client' => array(
-            'pretty_version' => '3.0.1',
-            'version' => '3.0.1.0',
+            'pretty_version' => '3.1.1',
+            'version' => '3.1.1.0',
             'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
